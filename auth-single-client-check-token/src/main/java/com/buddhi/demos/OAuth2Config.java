72e427e142092e2d5eac195daa838b3813165258
Buddhi Nagahawatte <SGARNABN@rhc.msig.com>
2019-09-30 10:22:57 +0800

    check token

--- auth-single-client-check-token/src/main/java/com/buddhi/demos/OAuth2Config.java
+++ auth-single-client-check-token/src/main/java/com/buddhi/demos/OAuth2Config.java
@@ -21,7 +21,7 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 				.inMemory()
 				.withClient("a")
 				.secret(passwordEncoder().encode("a"))
-				.authorities("ROLE_A","ROLE_B")
+				.authorities("ROLE_A","ROLE_B","ROLE_TRUSTED_CLIENT")
 				.scopes("all")
 				.authorizedGrantTypes("client_credentials")
 				.and()
@@ -35,10 +35,10 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 //		security.checkTokenAccess("denyAll()");//request: oauth/check_token response: {"timestamp":"2019-09-30T01:46:55.801+0000","status":403,"error":"Forbidden","message":"Forbidden","path":"/oauth/check_token"}
-		security.checkTokenAccess("permitAll()");//request: oauth/check_token response: {"scope":["all"],"active":true,"exp":1569851327,"authorities":["ROLE_A","ROLE_B"],"client_id":"a"}
-//		security.checkTokenAccess("isAnonymous()");
-//		security.checkTokenAccess("hasAuthority('ROLE_TRUSTED_CLIENT')");
-//		security.checkTokenAccess("hasAuthority('ROLE_B')");
+//		security.checkTokenAccess("permitAll()");//request: oauth/check_token response: {"scope":["all"],"active":true,"exp":1569851327,"authorities":["ROLE_A","ROLE_B"],"client_id":"a"}, can get token details with or without authentication
+//		security.checkTokenAccess("isAnonymous()");//without any authentication can request oauth/check_token
+		security.checkTokenAccess("hasAuthority('ROLE_TRUSTED_CLIENT')");//the user need to have ROLE_TRUSTED_CLIENT authority to request oauth/check_token ex. user only user "a" can request, user "b" doesn't has this authority
+//		security.checkTokenAccess("hasAuthority('ROLE_B')");//both user "a" and "b" can access
 //		security.tokenKeyAccess("isAnonymous() || hasAuthority('ROLE_TRUSTED_CLIENT')");
 	}
 
