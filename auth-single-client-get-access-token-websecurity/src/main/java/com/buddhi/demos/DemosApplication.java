7ace7eaf2e7eae7c4788516443e26fbfe964367b
Buddhi Nagahawatte <SGARNABN@rhc.msig.com>
2019-09-30 17:17:02 +0800

    securing controller method using WebSecurityConfigurerAdapter

--- auth-single-client-get-access-token-websecurity/src/main/java/com/buddhi/demos/DemosApplication.java
+++ auth-single-client-get-access-token-websecurity/src/main/java/com/buddhi/demos/DemosApplication.java
@@ -3,13 +3,21 @@ package com.buddhi.demos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableAuthorizationServer
+@RestController
 public class DemosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemosApplication.class, args);
 	}
 
+	@RequestMapping("/account")
+	public Account findAccount() {
+		return new Account(1, 1, "123456789", 1234);
+	}
 }
--- auth-single-client-get-access-token-websecurity/src/main/java/com/buddhi/demos/SecurityConfiguration.java
+++ auth-single-client-get-access-token-websecurity/src/main/java/com/buddhi/demos/SecurityConfiguration.java
@@ -14,6 +14,16 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().antMatchers("/account").permitAll().and()
+                // default protection for all resources (including /oauth/authorize)
+                .authorizeRequests()
+                .anyRequest().hasRole("USER");
+        // ... more configuration, e.g. for form login
+    }
+
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
@@ -22,7 +32,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         // remember the password that is printed out and use in the next step
 //        System.out.println(encoder.encode("a"));
         return new InMemoryUserDetailsManager(
-                User.withUsername("a")
+                User.withUsername("b")
                         .password("{bcrypt}$2a$10$oz72OLc5n/v2ej6rovd.WOt01S0v3Q2a.CGHRBJb6bzVSfS00j.Bi")
                         .roles("USER")
                         .build());
